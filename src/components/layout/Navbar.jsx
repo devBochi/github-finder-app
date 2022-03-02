954936516b5a765710255359afa1fb92b2b6b951
mdosuareznahuel <mdosuareznahuel@gmail.com>
2022-03-02 16:12:40 -0300

    trying again

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,4 @@
 import { FaGithub } from 'react-icons/fa'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
